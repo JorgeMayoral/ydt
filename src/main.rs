9d6b86a9ba84d218850361429d3528201cec7f32
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2024-06-13 18:49:54 +0200

    feat: :sparkles: add init cli command

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
+use clap::{Parser, Subcommand};
+
 mod commands;
 mod project;
 
-fn main() {
-    let project = commands::init();
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
 
-    println!("{project:#?}")
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Init => {
+            let project = commands::init();
+            println!("{project:#?}")
+        }
+    }
 }
